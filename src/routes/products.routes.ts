b1b99552ce32243d63a6e6481993e99c9e005c9d
fllantada <franciscollantada@gmail.com>
2022-10-17 22:42:43 -0300

    con algo de la sig entrega de la clase

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import productApi from "../controller/products";
+import { Request, Response } from "express";
+import path from "path";
 
 const productos = Router();
+const filePath = path.resolve(__dirname, "../../archivo.txt");
 
-productos.get("/", productApi.getAll);
+const getAllTest = (req: Request, res: Response) => {
+  productApi.getAll(req, res);
+};
+productos.get("/", getAllTest);
 
 export default productos;
